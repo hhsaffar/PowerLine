36ed62c5d96e587328cee4d1ab0fe7fb3605fddc
hhsaffar <hhsaffar@live.com>
2018-05-25 18:22:39 +0430

    making it more flexiable

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
-function init_game(){
+function init_game(game_container,row_cnt,col_cnt){
 
    log.log_control = document.getElementById('log');
    
-   new GameUI('gameMainArea',5,5);
-
+   new GameUI(game_container,row_cnt,col_cnt);
+   
 }
 
